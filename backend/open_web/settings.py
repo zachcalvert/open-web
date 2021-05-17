9072f110b143460ebea31db21eac65a63a264f65
Zach Calvert <zcalvert@oreilly.com>
2021-05-17 09:43:31 -0700

    remove sessionauthentication

--- backend/open_web/settings.py
+++ backend/open_web/settings.py
@@ -150,7 +150,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': [
         'rest_framework.filters.OrderingFilter',
